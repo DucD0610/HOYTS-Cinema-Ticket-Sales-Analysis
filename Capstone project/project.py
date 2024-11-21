ca1d53e12d35be3be349d05f51ce2e846295623f
DucD0610 <ddalex89@gmail.com>
2024-11-21 23:15:11 +1300

    data cleaning

--- Capstone project/project.py
+++ Capstone project/project.py
@@ -1,9 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pylot as plt
+import matplotlib as plt
 
 df_customer = pd.read_csv("D:\Python Learn\pandas_exercises-master\MazPythonData\Dataset\customer.csv")
-df_campaign = pd.read_csv("D:\Python Learn\pandas_exercises-master\MazPythonData\Dataset\customer.csv")
-df_device = pd.read_csv("D:\Python Learn\pandas_exercises-master\MazPythonData\Dataset\customer.csv")
-df_sales = pd.read_csv("D:\Python Learn\pandas_exercises-master\MazPythonData\Dataset\customer.csv")
-df_ticket = pd.read_csv("D:\Python Learn\pandas_exercises-master\MazPythonData\Dataset\customer.csv")
